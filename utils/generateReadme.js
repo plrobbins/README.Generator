7a452924511d6635e0c3a03813b386b640bc520d
Philip Robbins <plrobbins68@outlook.com>
2020-09-18 19:35:27 -0400

    updating the generateReadme after moving the code over. It was indented

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,37 +1,37 @@
 function generateReadme(data) {
-    const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbb9`;
-    return `
-    # ${data.title}
+const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbb9`;
+return `
+# ${data.title}
     
-    ## Description
-    ${data.description}
+## Description
+${data.description}
     
-    ## Table of Contents
-    - [Installation](#Installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Test] (#tests)
-    - [Contributors](#contributors)
-    - [Details](#details)
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#usage)
+- [License](#license)
+- [Test] (#tests)
+- [Contributors](#contributors)
+- [Details](#details)
     
-    ## Installation
-    Packages required to run this program are: ${data.installation}
+## Installation
+Packages required to run this program are: ${data.installation}
 
-    ## Usage
-    Examples of how to use this program: ${data.usage}
+## Usage
+Examples of how to use this program: ${data.usage}
 
-    ## License
-    ${data.license}
+## License
+${data.license}
 
-    ## Tests
-    To test, run the following program: ${data.test}
+## Tests
+To test, run the following program: ${data.test}
 
-    ## Contributors
-    ${data.contributor}
+## Contributors
+${data.contributor}
 
-    ## Contact
-    \n![Badge](${gitHub})
-    \nIf you have questions, contact the developer at: ${data.email}`
+## Contact
+\n![Badge](${gitHub})
+\nIf you have questions, contact the developer at: ${data.email}`
 
 }
 module.exports = generateReadme;
